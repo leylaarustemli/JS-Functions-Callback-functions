2b78c674dd24a2dd771d9b5d690ad1f2260be825
leylaarustemli <tu7h94maj@code.edu.az>
2024-04-08 02:05:52 +0400

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-// Reqemlerden ibaret arrayda duplicate reqemi qaytarin (bir funksiya teyin edirik ve parameter kimi reqemlerden ibaret array qebul edir. Arrayde yalniz bir eded duplicate olacaq) : 
+// ?Reqemlerden ibaret arrayda duplicate reqemi qaytarin (bir funksiya teyin edirik ve parameter kimi reqemlerden ibaret array qebul edir. Arrayde yalniz bir eded duplicate olacaq) :
 //   let myArr =  [1,3,4,2,2] // this is example. funksiya qaytarmalidir 2; Cunki 2 iki defe yazilib;
-//   let myArr =  [1,3,4,4,]; funksiya qaytarmalidir 4; cunki 4 iki defe yazilib.
-
-
-
-//   arr=[2,4,6,8,8,8,8]
-//   function duplicate(arr,tekrar){
-//     let say=0
-//     for (let i = 0; i < arr.length; i++) {
-//         if(tekrar==arr[i]){
-//             say++
-//         }
+// let arr = [1, 2, 3, 4, 6, 6];
+// function dublicate(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] == arr[j]) {
+//         return arr[i];
+//       }
 //     }
-//     return say
 //   }
-//   console.log(duplicate(arr,8))
+// }
+// console.log(dublicate(arr));
+
+// ?  Funksiya number fahrenheit parameter qebul eden funskiya yaziriq. Celsiusa convert edib return etsin.
+// function faranheytSelsi(cevirme) {
+//   let selsi = (5 / 9) * (cevirme - 32);
+//   return selsi;
+// }
+// console.log(faranheytSelsi(80));
 
 
-//   Funksiya number fahrenheit parameter qebul eden funskiya yaziriq. Celsiusa convert edib return etsin.
-function faranheytSelsi(cevirme){
-    let selsi=5/9*(faranheyt-32)
-    return selsi
-    
-}console.log(faranheytSelsi(cevirme));
